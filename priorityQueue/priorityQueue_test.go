d4737271acc3f12b707e74ba63c0fc0735a2f0e8
J-Catherine <865687826@qq.com>
2019-04-13 09:35:15 +0800

    priorityQueue_test

---
+++ priorityQueue/priorityQueue_test.go
@@ -0,0 +1,19 @@
+package priorityQueue
+
+import "testing"
+
+func TestNewPriorityQueue(t *testing.T) {
+	//h1 := NewPriorityQueue(1,1)
+	//h2:= NewPriorityQueue(5,0)
+}
+func TestPriorityQueue_Push(t *testing.T) {
+
+}
+
+func TestPriorityQueue_Pop(t *testing.T) {
+
+}
+
+func TestPriorityQueue_Top(t *testing.T) {
+
+}
