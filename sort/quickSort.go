6cad4bb0fc69de2576b3a4ef65d8c83ff7311c8c
J-Catherine <865687826@qq.com>
2019-04-14 14:16:29 +0800

    quickSort

--- sort/quickSort.go
+++ sort/quickSort.go
@@ -10,29 +10,25 @@ func QuickSort(nums Sortable)  {
 	length := nums.Len()
 	left,right := 0,length-1
 	rand.Seed(time.Now().Unix())
-	partition(nums,left,right)
+	qs(nums,left,right)
 
 }
 
-func partition(nums Sortable, start, end int)  {
+func qs(nums Sortable, start, end int)  {
 	if end <= start{
 		return
 	}
 	tmp := start + rand.Intn(end-start+1)
 	nums.Swap(start,tmp)
-	left, right := start+1, end
+	left, right := start, end
 	for ; left < right; {
-		for ;left < right && !nums.Less(right,start); right-=1{}
-		for ;left < right && nums.Less(left,start); left+=1{}
+		for ;left < right && nums.Less(start,right); right-=1{}
+		for ;left < right && !nums.Less(start,left); left+=1{}
 		nums.Swap(left, right)
 	}
-	if nums.Less(left,start) {
-		nums.Swap(left,start)
-	} else {
-		left -= 1
-	}
-	partition(nums,start,left-1)
-	partition(nums,left+1,end)
+	nums.Swap(left,start)
+	qs(nums,start,left-1)
+	qs(nums,left+1,end)
 
 }
 
